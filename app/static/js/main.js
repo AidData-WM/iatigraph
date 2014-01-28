c7d6a6259f18016518edabed5ad56cbc24352638
Phil Schleihauf <uniphil@gmail.com>
2014-01-28 13:25:08 -0500

    loads data for activity

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -24,7 +24,7 @@ site.Router = Backbone.Router.extend({
   // map #hash/urls to view functions
   // functions on this object will be called by the name they are mapped to
   routes: {
-    '': 'search',
+    '(search/:term)': 'search',
     'graph/:activity_id': 'graph',
   },
 
@@ -38,57 +38,22 @@ site.Router = Backbone.Router.extend({
     site.components.info.html(site.welcome_view.render().el);
   },
 
-  search: function() {
-    if (!site.search_view) {
-      site.search_view = new site.SearchView();
-      site.search_view.render();
-    }
-    site.components.map.html(site.search_view.el);
+  search: function(term) {
+    site.search_view = new site.SearchView();
+    site.components.map.html(site.search_view.render(term).el);
   },
 
   graph: function(activity_id) {
-    if (!site.graph_view) {
-      site.graph_view = new site.GraphView();
-    }
-    if (!site.info_view) {
-      site.info_view = new site.InfoView();
-    }
+    site.graph_view = new site.GraphView();
+    site.info_view = new site.InfoView();
+    $.getJSON('/activity/' + activity_id).then(function(data) {
+      console.log(data);
+    }, function(err) {
+      console.log(err);
+    });
     site.components.map.html(site.graph_view.render().el);
     site.components.info.html(site.info_view.render().el);
-    go_nodes();
-  },
-});
-
-
-//////////// View objects: handle the details of managing a view.
-/// entry points are generally either via the Router object,
-/// or events in the view while it is showing
-
-site.SearchView = Backbone.View.extend({
-  render: function() {
-    this.el = $(this.template());
-    return this;
-  },
-});
-
-site.WelcomeView = Backbone.View.extend({
-  render: function() {
-    this.el = $(this.template());
-    return this;
-  },
-});
-
-site.GraphView = Backbone.View.extend({
-  render: function() {
-    this.el = $(this.template());
-    return this;
-  },
-});
-
-site.InfoView = Backbone.View.extend({
-  render: function() {
-    this.el = $(this.template({project: {name: 'Some great project'}}));
-    return this;
+    go_nodes();  // this make the (sketchy) d3 stuff go... should be backbonified
   },
 });
 
