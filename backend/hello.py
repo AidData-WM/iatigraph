613f594a0e4c955cb2957ca98144b2d2d822ae96
Francis Kung <franciskung@ewb.ca>
2014-01-27 22:48:00 -0500

    More robust object oriented backend - easier to query and manipulate activities and transactions

--- backend/hello.py
+++ backend/hello.py
@@ -1,58 +1,26 @@
 from flask import Flask, jsonify
 
+from models import *
+from sample_data import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/activity/")
 @app.route("/activity/<id>")
-def show_activity(id):
-    
-    activity = {}
-    activity['id'] = 2015
+def show_activity(id=None):
 
-    activity['provider'] = []
+    if not id:
+        id = 2015       # just a default
 
-    p = {}
-    p['id'] = 1075;
-    p['amount'] = 132412
-    p['date'] = '2014-01-03'
-    activity['provider'].append(p)
+    if not sample_activity.get(id, None):
+        id = 2015       # will be a 404 error eventually...
 
-    p = {}
-    p['id'] = 1076;
-    p['amount'] = 876453
-    p['date'] = '2013-12-07'
-    activity['provider'].append(p)
-
-    activity['recipient'] = []
-
-    r = {}
-    r['id'] = 3112;
-    r['amount'] = 123
-    r['date'] = '2011-12-07'
-    activity['recipient'].append(r)
-
-    r = {}
-    r['id'] = 3113;
-    r['amount'] = 112323
-    r['date'] = '2012-08-07'
-    activity['recipient'].append(r)
-
-    r = {}
-    r['id'] = 3114;
-    r['amount'] = 64535
-    r['date'] = '2014-01-07'
-    activity['recipient'].append(r)
-
-    activity['name'] = 'This is an activity!'
-    activity['date'] = '2014-01-27'
-    activity['status'] = 'Implementation'
-    activity['recipient_country'] = 'ZM'
-    activity['sector'] = 'Food aid/Food security programmes'
-    
-    return jsonify(results = activity)
+    return jsonify(results = sample_activity[id].to_array())
+    # we use an array right now, but this could easily turn into an ORM query
 
 if __name__ == "__main__":
     app.run(debug=True)
